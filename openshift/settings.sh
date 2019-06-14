b49f352717a2ea23f86d2a8dc84192b4ef9d21a2
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-14 16:00:23 -0700

    Created OpenShift configuration

---
+++ openshift/settings.sh
@@ -0,0 +1,21 @@
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-devex-von-permitify}
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/von-oil-gas-poc.git"}
+export GIT_REF=${GIT_REF:-"master"}
+
+
+# The project components
+# - They are all contained under the main OpenShift folder.
+export components="."
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden with the values of GIT_URI and GIT_REF
+export -a skip_git_overrides=""
+
+# The builds to be triggered after buildconfigs created (not auto-triggered)
+export builds=""
+
+# The images to be tagged after build
+export images="bc-oil-and-gas"
+
+# The routes for the project
+export routes="bc-oil-and-gas"
